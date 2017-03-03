ec956307287bdfe658a250322022453785bbed12
FacundoMierez <facundomierez@gmail.com>
2017-03-03 16:46:07 -0300

    agregando menu

--- TecnoDeezer/src/app/app.component.ts
+++ TecnoDeezer/src/app/app.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component ,ViewChild} from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
+/*Paginas personales*/
 import { HomePage } from '../pages/home/home';
-
+import { PerfilesPage } from '../pages/perfiles/perfiles';
+import { ContactosPage } from '../pages/contactos/contactos';
+import { AcercaPage } from '../pages/acerca/acerca';
+/*Paginas personales*/
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  @ViewChild('NAV') nav: Nav;
+
+  public rootPage : any;
+
+  public pages:Array<{titulo:string,component:any,icon:string}>;
 
   constructor(platform: Platform) {
+    this.rootPage=HomePage;
+    this.pages=[
+              {titulo:'Inicio',component:HomePage,icon:'home'},
+              {titulo:'Perfiles Deezer',component:PerfilesPage,icon:'person'},
+              {titulo:'Contactos',component:ContactosPage,icon:'contact'},
+              {titulo:'Acerca de',component:AcercaPage,icon:'information-circle'}
+             ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  goToPage(page){
+    this.nav.setRoot(page);
   }
 }
--- TecnoDeezer/src/app/app.module.ts
+++ TecnoDeezer/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+
+/*Page personales*/
+
 import { HomePage } from '../pages/home/home';
+import { PerfilesPage } from '../pages/perfiles/perfiles';
+import { ContactosPage } from '../pages/contactos/contactos';
+import { AcercaPage } from '../pages/acerca/acerca';
+
+/*Page personales*/
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PerfilesPage,
+    ContactosPage,
+    AcercaPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PerfilesPage,
+    ContactosPage,
+    AcercaPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
